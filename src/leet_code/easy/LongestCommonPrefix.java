f1ec0d1f4bbf0d8dafab35acf80fa467484ab297
dima <cinnamonbun1233@yandex.ru>
2023-09-08 10:24:55 +0300

    Update

---
+++ src/leet_code/easy/LongestCommonPrefix.java
@@ -0,0 +1,39 @@
+package leet_code.easy;
+
+import java.util.Arrays;
+
+/**
+ * leetcode.com/problems/longest-common-prefix/
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+
+        String[] array = {"dog", "racecar", "car"};
+        String result = LongestCommonPrefix.longestCommonPrefix(array);
+        System.out.println(result);
+    }
+}
+
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] s) {
+
+        Arrays.sort(s);
+        String s1 = s[0];
+        String s2 = s[s.length - 1];
+        int index = 0;
+
+        while (index < s1.length() && index < s2.length()) {
+
+            if (s1.charAt(index) == s2.charAt(index)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        return s1.substring(0, index);
+    }
+}
